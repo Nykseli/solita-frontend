80687069b22b5984fe9167402a4bac6da741794a
Miika Alikirri <miika.alik@gmail.com>
2021-01-11 07:43:56 +0200

    Expanded eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,22 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/no-use-v-if-with-v-for": "off",
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-unused-labels": "off"
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        mocha: true
+      }
+    }
+  ]
 };
