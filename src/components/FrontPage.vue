cc96a09071f78a5062dac8ec289a77346edd528d
Miika Alikirri <miika.alik@gmail.com>
2021-01-11 15:48:44 +0200

    Redesigned the front page
    The front page now has information about the project

---
+++ src/components/FrontPage.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="front-page">
+    <h1>Welcome to the Solita dev academy demo application</h1>
+    <p>
+      This site contains a page for inspecting the name data with a simple table
+      view.
+      <br />
+      And a page for testing the rest api.
+    </p>
+    <v-container>
+      <v-row class="mb-6">
+        <v-col>
+          <h3>Frontend techologies</h3>
+          <ul>
+            <li>
+              <a href="https://vuejs.org/" target="_blank" rel="noopener">
+                Vue.js
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://vuetifyjs.com/en/"
+                target="_blank"
+                rel="noopener"
+              >
+                Vuetify
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.typescriptlang.org/"
+                target="_blank"
+                rel="noopener"
+              >
+                Typescript
+              </a>
+            </li>
+          </ul>
+        </v-col>
+        <v-col>
+          <h3>Backend techologies</h3>
+          <ul>
+            <li>
+              <a href="https://golang.org/" target="_blank" rel="noopener">
+                golang
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/gorilla/mux"
+                target="_blank"
+                rel="noopener"
+              >
+                gorilla/mux
+              </a>
+            </li>
+          </ul>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class FrontPage extends Vue {}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+/* ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+} */
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <FrontPage />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import FrontPage from "@/components/FrontPage.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    FrontPage
   }
 })
 export default class Home extends Vue {}
